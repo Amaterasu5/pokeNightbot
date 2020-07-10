4ad060c0bb88141d340aaeab8492196149d6c070
Andre Mroz <mathmonkey4@gmail.com>
2020-07-09 22:31:24 -0600

    so much omg

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const mainFunctions = require('./main.js');
+const { JSDOM } = require( "jsdom" );
+const { window } = new JSDOM( "" );
+const $ = require( "jquery" )( window );
+const app = express();
+app.use(bodyParser.json());
+app.use(cookieParser());
+const url = require('url');
+
+app.get('/', (req, res) => {
+  var searchQuery = req.originalUrl;
+  searchQuery = searchQuery.charAt(0)=='/'? searchQuery.substring(1) : searchQuery;
+  let params = new URLSearchParams(searchQuery);
+  let pdata = params.get('data');
+  let pdata8 = pdata.replace(/-/gi,'_');
+  (async function(){
+    let info = await mainFunctions.displayData(pdata,pdata8);
+    res.send(info);
+  })();
+});
+
+app.listen(8000, () => {
+  console.log('Example app listening on port 8000!')
+});
--- main.js
+++ main.js
@@ -1,17 +1,13 @@
-$(document).ready(function(){
-  $("body").empty();
-  let params = new URLSearchParams(window.location.search);
-  let pdata = params.get('data');
-  //let pdata = "";
-  let pdata8 = pdata.replace(/-/gi,'_');
+  const { JSDOM } = require( "jsdom" );
+  const { window } = new JSDOM( "" );
+  const $ = require( "jquery" )( window );
 
-  let allPokemon=null;
-  let galar=null;
-  let galarMoves=null;
+  let allPokemon = null;
+  let galar = null;
+  let galarMoves = null;
   let allMoves = null;
 
-  var promises = [Promise.resolve()];
-  promise1 = new Promise((resolve,reject) => {
+  var promise1 = new Promise((resolve,reject) => {
     $.ajax({
       url:'https://pokeapi.co/api/v2/pokemon/?limit=1000',
       dataType:'json',
@@ -21,7 +17,7 @@ $(document).ready(function(){
       }
     });
   });
-  promise2 = new Promise((resolve,reject) => {
+  var promise2 = new Promise((resolve,reject) => {
     $.ajax({
       url:'https://api.jsonbin.io/b/5ef40ab9e2ce6e3b2c792e97/4',
       dataType:'json',
@@ -31,7 +27,7 @@ $(document).ready(function(){
       }
     });
   });
-  promise3 = new Promise((resolve,reject) => {
+  var promise3 = new Promise((resolve,reject) => {
     $.ajax({
       url:'https://api.jsonbin.io/b/5efea94f0bab551d2b6b08c3/1',
       dataType:'json',
@@ -41,7 +37,7 @@ $(document).ready(function(){
       }
     });
   });
-  promise4 = new Promise((resolve,reject) => {
+  var promise4 = new Promise((resolve,reject) => {
     $.ajax({
       url:'https://pokeapi.co/api/v2/move/?limit=800',
       dataType:'json',
@@ -52,17 +48,17 @@ $(document).ready(function(){
     });
   });
 
-  async function displayData(){
+  var methods = {};
+  methods.displayData = async function(pdata,pdata8){
     var promises = [promise1,promise2,promise3,promise4];
     var unneeded = await Promise.all(promises); //wait for the "databases" to populate
-
     let thisUrl=null;
     let apiData=null;
     thisPokemon = allPokemon.find(element => element.name == pdata);
     thisMove = allMoves.find(element => element.name == pdata);
 
     if (galar[pdata8]!=undefined){
-      displayPokemon(galar[pdata8].stats);
+      return displayPokemon(galar[pdata8].stats);
     }else if (thisPokemon!=undefined){
       thisUrl = thisPokemon.url;
       var promise6 = new Promise((resolve,reject) => {
@@ -76,9 +72,9 @@ $(document).ready(function(){
         });
       });
       await Promise.all([promise6]);
-      displayPokemon(apiData);
+      return displayPokemon(apiData);
     }else if (galarMoves[pdata8]!=undefined){
-      displayMove(galarMoves[pdata8]);
+      return displayMove(galarMoves[pdata8]);
     }else if (thisMove!=undefined){
       thisUrl = thisMove.url;
       var promise7 = new Promise((resolve,reject) => {
@@ -92,25 +88,26 @@ $(document).ready(function(){
         });
       });
       await Promise.all([promise7]);
-      displayMove(apiData);
+      return displayMove(apiData);
     }else{//when people put in bullshit or spell things wrong
-      $('body').text("lol wtf was that");
+      return "lol wtf was that";
     }
   }
 
-  displayData();
-})
+  //displayData();
 
 function displayPokemon(pokedata){
   items=[];
   for (i=0;i<6;i++){
     items.push(' '+pokedata[i].stat.name+': '+ pokedata[i].base_stat);
   }
-  $('body').text(items);
+  return items;
 }
 
 function displayMove(movedata){
    items=[];
    items.push('Type: '+movedata.type.name+', Power: '+movedata.power+', Accuracy: '+movedata.accuracy+', Priority: '+ movedata.priority+ ', Max PP: '+parseInt(movedata.pp,10)*(8.0/5)+', Damage type: '+movedata.damage_class.name+ ', '+movedata.effect_entries[0].short_effect);
-   $('body').text(items);
+   return items;
 }
+
+module.exports = methods;
