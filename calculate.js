0e5952de032251e06174238dea9e83b8f928ad22
Andre Mroz <mathmonkey4@gmail.com>
2020-08-07 20:01:55 -0600

    added field effects

--- calculate.js
+++ calculate.js
@@ -1,5 +1,4 @@
 const smogon = require('@smogon/calc');
-//import {calculate, Generations, Pokemon, Move, Field} from smogon;
 
 const gen = smogon.Generations.get(8);
 
@@ -23,6 +22,9 @@ methods.performCalc = function(attacker,defender,move,field){
     level:50,
     isDynamaxed:defender.dynamax || false
   });
+  moveCalc = new smogon.Move(gen,move,{
+    useMax:attackerCalc.isDynamaxed
+  });
   fieldCalc = new smogon.Field({
     gameType:field.singleTarget? 'Singles':'Doubles',
     terrain:field.terrain,
@@ -31,7 +33,79 @@ methods.performCalc = function(attacker,defender,move,field){
     attackerSide:field.attackerSide,
     defenderSide:field.defenderSide
   });
-  result = smogon.calculate(gen, attackerCalc,defenderCalc,new smogon.Move(gen, move),fieldCalc);
+  result = smogon.calculate(gen, attackerCalc,defenderCalc,moveCalc,fieldCalc);
   return result.desc();
 }
+
+function capitalizeWords(string){
+  words=string.split(' ');
+  for(i=0;i<words.length;i++){
+    words[i]=words[i].charAt(0).toUpperCase()+words[i].substring(1);
+  }
+  return words.join(' ');
+}
+
+methods.setUpPokemon = function(data){
+  const stats=['hp','atk','def','spa','spd','spe'];
+  const statChange={spatk:'spa',spdef:'spd',speed:'spe'};
+  monEvs={};
+  for(let item of data){
+    for(let stat of stats){
+      if(statChange.hasOwnProperty(item.substring(0,5))){
+        item=statChange[item.substring(0,5)]+item.substring(5);
+      }
+      if(item.includes(stat)){
+        monEvs[stat]=item.replace(stat+'=','');
+      }
+    }
+  }
+  item=data.find(element=>element.includes('item='));
+  nature=data.find(element =>element.includes('nature='));
+  boosts=data.find(element=> element.charAt(0)=="+"||element.charAt(0)=='-');
+  pokemon={
+    name:capitalizeWords(data[0]),
+    item:item?capitalizeWords(item.substring(5).replace(/-/gi,' ')):null,
+    nature:nature?nature.substring(7):null,
+    boosts:boosts?boosts.substring(1):null,
+    evs:monEvs,
+    dynamax:data.some(element => element.includes('dynamax'))
+  };
+  return pokemon;
+}
+
+methods.setUpField = function(data){
+  const terrains = ['grassy','misty','psychic','electric'];
+  const weathers = ['sand','hail','sun','rain','harsh sunlight','heavy rain','strong winds'];
+  let weatherData = null;
+  for (let weather of weathers){
+    for (let item of data){
+      if (item.replace(/-/gi,' ')==weather){
+        weatherData=weather;
+      }
+    }
+  }
+  let switching = data.find(element=>element.includes('switch-'));
+  attackerSide = {
+    isTailwind:data.some(element=>element.includes('tailwind')),
+    isHelpingHand:data.some(element=>element.includes('helping-hand')||element.includes('hh')),
+    isBattery:data.some(element=>element.includes('battery'))
+  };
+  defenderSide = {
+    isReflect:data.some(element=>element.includes('reflect')),
+    isLightScreen:data.some(element=>element.includes('light-screen')),
+    isFriendGuard:data.some(element=>element.includes('friend-guard')),
+    isAuroraVeil:data.some(element=>element.includes('aurora-veil')),
+    isSwitching:switching? switching.replace('switch-',''):undefined
+  };
+  field={
+    singleTarget:data.some(element=>element.includes('single')),
+    terrain:data.find(element=>terrains.includes(element)),
+    weather:weatherData? capitalizeWords(weatherData):null,
+    gravity:data.some(element=>element.includes('gravity')),
+    attackerSide:attackerSide,
+    defenderSide:defenderSide
+  };
+  return field;
+}
+
 module.exports = methods;
--- index.js
+++ index.js
@@ -63,11 +63,13 @@ app.get('/calc',(req,res) => {
   var searchQuery = req.originalUrl.replace(req.path,'');
   let params = new URLSearchParams(searchQuery);
   attackerData = params.get('attacker').split(',');
-  attacker = setUpPokemon(attackerData);
+  attacker = calculate.setUpPokemon(attackerData);
   defenderData = params.get('defender').split(',');
-  defender = setUpPokemon(defenderData);
-  move=params.get('move').replace(/-/,' ');
-  field={};
+  defender = calculate.setUpPokemon(defenderData);
+  move = params.get('move').replace(/-/,' ');
+  fieldData = params.get('field');
+  field = fieldData? calculate.setUpField(fieldData.split(',')):{};
+  //field = {};
   result = calculate.performCalc(attacker,defender,move,field);
   if(result){
     res.send([result]);
@@ -75,43 +77,5 @@ app.get('/calc',(req,res) => {
     res.send("sorry boss idk");
   }
 });
-
-function capitalizeWords(string){
-  words=string.split(' ');
-  for(i=0;i<words.length;i++){
-    words[i]=words[i].charAt(0).toUpperCase()+words[i].substring(1);
-  }
-  return words.join(' ');
-}
-
-function setUpPokemon(data){
-  const stats=['hp','atk','def','spa','spd','spe'];
-  const statChange={spatk:'spa',spdef:'spd',speed:'spe'};
-  monEvs={};
-  for(let item of data){
-    for(let stat of stats){
-      //console.log(item);
-      if(statChange.hasOwnProperty(item.substring(0,5))){
-        item=statChange[item.substring(0,5)]+item.substring(5);
-      }
-      if(item.includes(stat)){
-        monEvs[stat]=item.replace(stat+'=','');
-      }
-    }
-  }
-  item=data.find(element=>element.includes('item='));
-  nature=data.find(element =>element.includes('nature='));
-  boosts=data.find(element=> element.charAt(0)=="+"||element.charAt(0)=='-');
-  pokemon={
-    name:capitalizeWords(data[0]),
-    item:item?capitalizeWords(item.substring(5).replace(/-/gi,' ')):null,
-    nature:nature?nature.substring(7):null,
-    boosts:boosts?boosts.substring(1):null,
-    evs:monEvs,
-    dynamax:data.some(element => element.includes('dynamax'))
-  };
-  console.log(pokemon.dynamax);
-  return pokemon;
-}
 
 app.listen(process.env.PORT||5000);
